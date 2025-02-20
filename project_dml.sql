7f3d03a801724a8a4ddcce7d3bfeefc6d3bbfb2c
Vinícius Moreira <vsmoreira@gmail.com>
2025-02-20 10:49:07 -0300

    Tabela de respostas

--- project_dml.sql
+++ project_dml.sql
@@ -1,3 +1,46 @@
+/*
+ domain tables
+*/
+create table tipo_dependencia_administrativa (
+	cd_tipo_dep_adm integer primary key,
+	descricao varchar(25) not null
+);
+
+insert into tipo_dependencia_administrativa (cd_tipo_dep_adm, descricao ) values
+(1,	'Federal'),
+(2,	'Estadual'),
+(3,	'Municipal'),
+(4,	'Privada');
+
+/**
+ * business tables
+ */
+create table inep.escola as
+	select
+	distinct
+		me.co_municipio_esc,
+		me.no_municipio_esc,
+		me.co_uf_esc,
+		me.tp_dependencia_adm_esc,
+		me.tp_localizacao_esc,
+		me.tp_sit_func_esc
+	from
+		microdados_enem me
+	where
+		me.co_municipio_esc is not null;
+alter table inep.escola add column id_escola serial primary key;
+
+create table inep.local_prova as
+select
+	distinct
+		me.co_municipio_prova,
+		me.no_municipio_prova,
+		me.co_uf_prova,
+		me.sg_uf_prova
+	from
+		microdados_enem me;
+alter table inep.local_prova add column id_local_prova serial primary key;
+
 create table inep.participante as
 	select
 		me.nu_inscricao::bigint,
@@ -18,35 +61,6 @@ alter table inep.participante add column id_participante bigserial primary key;
 alter table inep.participante add unique (nu_inscricao, nu_ano);
 alter table inep.participante add COLUMN id_escola integer;
 alter table inep.participante add COLUMN id_local_prova integer;
-alter table inep.participante add foreign key (id_escola) references inep.escola(id_escola);
-alter table inep.participante add foreign key (id_local_prova) references inep.local_prova(id_local_prova);
-
-create table inep.escola as
-	select
-	distinct
-		me.co_municipio_esc,
-		me.no_municipio_esc,
-		me.co_uf_esc,
-		me.tp_dependencia_adm_esc,
-		me.tp_localizacao_esc,
-		me.tp_sit_func_esc
-	from
-		microdados_enem me
-	where
-		me.co_municipio_esc is not null;
-
-alter table inep.escola add column id_escola serial primary key;
-
-create table inep.local_prova as
-select
-	distinct
-		me.co_municipio_prova,
-		me.no_municipio_prova,
-		me.co_uf_prova,
-		me.sg_uf_prova
-	from
-		microdados_enem me;
-alter table inep.local_prova add column id_local_prova serial primary key;
 
 update participante p set id_escola = e.id_escola 
 	from
@@ -73,3 +87,87 @@ update participante p set id_local_prova = e.id_local_prova
 	where
 		(p.nu_inscricao = me.nu_inscricao::bigint
 			and p.nu_ano = me.nu_ano);
+
+create table resposta_participante (
+	id_resposta bigserial primary key,
+	id_participante bigint not null,
+	tp_prova char(2) not null,
+	co_prova int null,
+	tp_presenca int not null,
+	nu_nota float null,
+	tx_resposta varchar(50) null,
+	tp_lingua int
+);
+
+/**
+ * insert data
+ */
+
+insert into resposta_participante (id_participante, tp_prova, co_prova, tp_presenca, nu_nota, tx_resposta, tp_lingua)
+	select
+			p.id_participante,
+			'CN' as tp_prova,
+			me.co_prova_cn as co_prova,
+			me.tp_presenca_cn as tp_presenca,
+			me.nu_nota_cn as nu_nota,
+			me.tx_respostas_cn as tx_resposta,
+			-1 as tp_lingua
+		from
+			microdados_enem me
+		inner join
+			participante p on (p.nu_inscricao = me.nu_inscricao::bigint and p.nu_ano = me.nu_ano)
+	
+		union
+			
+		select
+			p.id_participante,
+			'CH' as tp_prova,
+			me.co_prova_ch  as co_prova,
+			me.tp_presenca_ch  as tp_presenca,
+			me.nu_nota_ch  as nu_nota,
+			me.tx_respostas_ch  as tx_respostas,
+			-1 as tp_lingua
+		from
+			microdados_enem me
+		inner join
+			participante p on (p.nu_inscricao = me.nu_inscricao::bigint and p.nu_ano = me.nu_ano)
+		
+		union
+			
+		select
+			p.id_participante,
+			'LC' as tp_prova,
+			me.co_prova_lc as co_prova,
+			me.tp_presenca_lc as tp_presenca,
+			me.nu_nota_lc   as nu_nota,
+			me.tx_respostas_lc as tx_respostas,
+			me.tp_lingua
+		from
+			microdados_enem me
+		inner join
+			participante p on (p.nu_inscricao = me.nu_inscricao::bigint and p.nu_ano = me.nu_ano)
+			
+		union
+		
+		select
+			p.id_participante,
+			'MT' as tp_prova,
+			me.co_prova_mt as co_prova,
+			me.tp_presenca_mt as tp_presenca,
+			me.nu_nota_mt as nu_nota,
+			me.tx_respostas_mt as tx_respostas,
+			-1 as tp_lingua
+		from
+			microdados_enem me
+		inner join
+			participante p on (p.nu_inscricao = me.nu_inscricao::bigint and p.nu_ano = me.nu_ano)
+;
+
+/**
+ * constraionts
+ */
+alter table inep.escola add foreign key (tp_dependencia_adm_esc) references tipo_dependencia_administrativa(cd_tipo_dep_adm);
+alter table inep.participante add foreign key (id_escola) references inep.escola(id_escola);
+alter table inep.participante add foreign key (id_local_prova) references inep.local_prova(id_local_prova);
+alter table inep.resposta_participante add foreign key (id_participante) references inep.participante(id_participante);
+alter table inep.resposta_participante add foreign key (co_prova) references inep.gabarito(co_prova);
